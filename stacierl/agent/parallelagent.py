13ab06d4cb3a730ff1adbc193fcef8c87ddbf257
Lennart Karstensen <lennart.karstensen@ipa.fraunhofer.de>
2021-08-04 11:53:28 +0200

    parallelagent update

--- stacierl/agent/parallelagent.py
+++ stacierl/agent/parallelagent.py
@@ -9,6 +9,7 @@ from ..environment import Environment, EnvFactory
 from torch import multiprocessing as mp
 from math import inf, ceil
 import numpy as np
+import torch
 
 
 class SingleAgentProcess(mp.Process, SingleAgent):
@@ -27,6 +28,7 @@ class SingleAgentProcess(mp.Process, SingleAgent):
         self._shutdown_event = mp.Event()
         self._task_queue = mp.Queue()
         self._result_queue = mp.Queue()
+        self._model_queue = mp.SimpleQueue()
 
         self.explore_step_counter_mp = mp.Value("i", 0)
         self.explore_episode_counter_mp = mp.Value("i", 0)
@@ -52,8 +54,19 @@ class SingleAgentProcess(mp.Process, SingleAgent):
                 self.eval_step_counter_mp.value = self.eval_step_counter
                 self.eval_episode_counter_mp.value = self.eval_episode_counter
             elif task_name == "update":
-                result = self._update(task[1], task[2])
+                self._update(task[1], task[2])
                 self.update_step_counter_mp.value = self.update_step_counter
+                device = self.algo.model.device
+                self.algo.to(torch.device("cpu"))
+                self._model_queue.put(self.algo.model.all_state_dicts())
+                self.algo.to(device)
+                continue
+            elif task_name == "set_all_state_dicts":
+                device = self.algo.model.device
+                self.algo.to(torch.device("cpu"))
+                result = self.algo.model.load_all_state_dicts(task[1])
+                self.algo.to(device)
+                continue
             self._result_queue.put(result)
 
         while True:
@@ -80,8 +93,14 @@ class SingleAgentProcess(mp.Process, SingleAgent):
     def update(self, steps, batch_size):
         self._task_queue.put(["update", steps, batch_size])
 
-    def get_results(self):
+    def set_all_state_dicts(self, all_state_dicts):
+        self._task_queue.put(["set_all_state_dicts", all_state_dicts])
+
+    def get_result(self):
         return self._result_queue.get()
+
+    def get_state_dict(self):
+        return self._model_queue.get()
 
     def shutdown(self):
         self._shutdown_event.set()
@@ -95,25 +114,33 @@ class ParallelAgent(Agent):
         env_factory: EnvFactory,
         replay_buffer: ReplayBuffer,
         consecutive_action_steps: int = 1,
+        model_update_per_agent_tau=0.35,
     ) -> None:
 
         self.algo = algo
+        self.dummy_algo = self.algo.copy()
+        # self.dummy_algo.model.to(self.algo.device)
         self.env_factory = env_factory
         self.replay_buffer = replay_buffer
         self.consecutive_action_steps = consecutive_action_steps
         self.n_agents = n_agents
+        self.model_update_per_agent_tau = model_update_per_agent_tau
 
         self.agents: List[SingleAgentProcess] = []
 
         for _ in range(n_agents):
+            algo_copy = self.algo.copy()
+            algo_copy.to(self.algo.device)
             self.agents.append(
                 SingleAgentProcess(
-                    self.algo.copy_shared_memory(),
+                    algo_copy,
                     self.env_factory.create_env(),
                     replay_buffer.copy(),
                     consecutive_action_steps,
                 )
             )
+
+        self.algo.to(torch.device("cpu"))
 
         for agent in self.agents:
             agent.start()
@@ -122,9 +149,15 @@ class ParallelAgent(Agent):
         steps_per_agent = ceil(steps / self.n_agents)
         for agent in self.agents:
             agent.update(steps_per_agent, batch_size)
-        results = []
         for agent in self.agents:
-            results.append(agent.get_results())
+            state_dict = agent.get_state_dict()
+            self.dummy_algo.model.load_all_state_dicts(state_dict)
+            all_parameters = self.dummy_algo.model.all_parameters()
+            self.algo.model.soft_tau_update_all(all_parameters, self.model_update_per_agent_tau)
+
+        all_state_dicts = self.algo.model.all_state_dicts()
+        for agent in self.agents:
+            agent.set_all_state_dicts(all_state_dicts)
 
     def _heatup(self, steps: int = None, episodes: int = None) -> Tuple[float, float]:
         steps_per_agent, episodes_per_agent = self._divide_steps_and_episodes(steps, episodes)
@@ -132,7 +165,7 @@ class ParallelAgent(Agent):
             agent.heatup(steps_per_agent, episodes_per_agent)
         results = []
         for agent in self.agents:
-            results.append(agent.get_results())
+            results.append(agent.get_result())
 
         results = np.array(results)
         results = np.mean(results, axis=0)
@@ -144,7 +177,7 @@ class ParallelAgent(Agent):
             agent.explore(steps_per_agent, episodes_per_agent)
         results = []
         for agent in self.agents:
-            results.append(agent.get_results())
+            results.append(agent.get_result())
 
         results = np.array(results)
         results = np.mean(results, axis=0)
@@ -156,7 +189,7 @@ class ParallelAgent(Agent):
             agent.evaluate(steps_per_agent, episodes_per_agent)
         results = []
         for agent in self.agents:
-            results.append(agent.get_results())
+            results.append(agent.get_result())
 
         results = np.array(results)
         results = np.mean(results, axis=0)

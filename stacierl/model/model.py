a73591cfcba480190da1b1e087d8b5176ed7396e
Lennart Karstensen <lennart.karstensen@ipa.fraunhofer.de>
2021-08-06 16:28:31 +0200

    implemented model sharing methods. Adjusted Parallel Agent

--- stacierl/model/model.py
+++ stacierl/model/model.py
@@ -2,6 +2,133 @@ from abc import ABC, abstractmethod
 from typing import Optional, Tuple
 import numpy as np
 import torch
+from dataclasses import dataclass
+
+
+@dataclass
+class ModelStateDicts(ABC):
+    @abstractmethod
+    def __iter__(self):
+        ...
+
+    @abstractmethod
+    def copy(self):
+        ...
+
+    def to(self, device: torch.device):
+        for state_dict in self:
+            for tensor_name, tensor in state_dict.items():
+                state_dict[tensor_name] = tensor.to(device)
+
+    def __add__(self, other):
+        copy = self.copy()
+        if isinstance(other, float) or isinstance(other, int):
+            for state_dict in copy:
+                for tensor in state_dict.values():
+                    tensor.data.copy_(tensor + other)
+        if isinstance(other, self.__class__):
+            for copy_state_dict, other_state_dict in zip(copy, other):
+                for own_tensor, other_tensor in zip(
+                    copy_state_dict.values(), other_state_dict.values()
+                ):
+                    own_tensor.data.copy_(own_tensor + other_tensor)
+        return copy
+
+    def __iadd__(self, other):
+        if isinstance(other, float) or isinstance(other, int):
+            for state_dict in self:
+                for tensor in state_dict.values():
+                    tensor.data.copy_(tensor + other)
+        if isinstance(other, self.__class__):
+            for copy_state_dict, other_state_dict in zip(self, other):
+                for own_tensor, other_tensor in zip(
+                    copy_state_dict.values(), other_state_dict.values()
+                ):
+                    own_tensor.data.copy_(own_tensor + other_tensor)
+        return self
+
+    def __mul__(self, other):
+        copy = self.copy()
+        if isinstance(other, float) or isinstance(other, int):
+            for state_dict in copy:
+                for tensor in state_dict.values():
+                    tensor.data.copy_(tensor * other)
+        if isinstance(other, self.__class__):
+            for copy_state_dict, other_state_dict in zip(copy, other):
+                for copy_tensor, other_tensor in zip(
+                    copy_state_dict.values(), other_state_dict.values()
+                ):
+                    copy_tensor.data.copy_(copy_tensor * other_tensor)
+        return copy
+
+    def __imul__(self, other):
+        if isinstance(other, float) or isinstance(other, int):
+            for state_dict in self:
+                for tensor in state_dict.values():
+                    tensor.data.copy_(tensor * other)
+        if isinstance(other, self.__class__):
+            for copy_state_dict, other_state_dict in zip(self, other):
+                for copy_tensor, other_tensor in zip(
+                    copy_state_dict.values(), other_state_dict.values()
+                ):
+                    copy_tensor.data.copy_(copy_tensor * other_tensor)
+        return self
+
+    def __truediv__(self, other):
+        copy = self.copy()
+        if isinstance(other, float) or isinstance(other, int):
+            for state_dict in copy:
+                for tensor in state_dict.values():
+                    tensor.data.copy_(tensor / other)
+        if isinstance(other, self.__class__):
+            for copy_state_dict, other_state_dict in zip(copy, other):
+                for copy_tensor, other_tensor in zip(
+                    copy_state_dict.values(), other_state_dict.values()
+                ):
+                    copy_tensor.data.copy_(copy_tensor / other_tensor)
+        return copy
+
+    def __itruediv__(self, other):
+        if isinstance(other, float) or isinstance(other, int):
+            for state_dict in self:
+                for tensor in state_dict.values():
+                    tensor.data.copy_(tensor / other)
+        if isinstance(other, self.__class__):
+            for copy_state_dict, other_state_dict in zip(self, other):
+                for copy_tensor, other_tensor in zip(
+                    copy_state_dict.values(), other_state_dict.values()
+                ):
+                    copy_tensor.data.copy_(copy_tensor / other_tensor)
+        return self
+
+
+@dataclass
+class ModelNetworks(ABC):
+    @abstractmethod
+    def to(self, device: torch.device) -> None:
+        ...
+
+    @abstractmethod
+    def soft_tau_update(self, model_parameters, tau: float):
+        ...
+
+    @abstractmethod
+    def load_state_dicts(self, model_state_dicts):
+        ...
+
+    @property
+    @abstractmethod
+    def state_dicts(self):
+        ...
+
+    @abstractmethod
+    def __iter__(self):
+        ...
+
+    def to(self, device: torch.device) -> None:
+        self.device = device
+        for net in self:
+            net.to(device)
 
 
 class Model(ABC):
@@ -28,18 +155,7 @@ class Model(ABC):
     def initial_hidden_state(self) -> Optional[torch.Tensor]:
         ...
 
+    @property
     @abstractmethod
-    def all_state_dicts(self):
-        ...
-
-    @abstractmethod
-    def all_parameters(self):
-        ...
-
-    @abstractmethod
-    def load_all_state_dicts(self, all_state_dicts: dict):
-        ...
-
-    @abstractmethod
-    def soft_tau_update_all(self, all_parameters: dict, tau: float):
+    def nets(self) -> ModelNetworks:
         ...
--- stacierl/model/sac.py
+++ stacierl/model/sac.py
@@ -1,11 +1,74 @@
-from typing import List, Optional, Tuple
+from typing import Any, Dict, Generator, List, Optional, OrderedDict, Tuple
 import numpy as np
-
 from torch.distributions.normal import Normal
-from .model import Model
+from .model import Model, ModelNetworks, ModelStateDicts
 from .. import network
 import torch.optim as optim
 import torch
+from dataclasses import dataclass
+from copy import deepcopy
+
+
+@dataclass
+class SACModelStateDicts(ModelStateDicts):
+    q_net_1: Dict[str, torch.Tensor]
+    q_net_2: Dict[str, torch.Tensor]
+    target_q_net_1: Dict[str, torch.Tensor]
+    target_q_net_2: Dict[str, torch.Tensor]
+    policy_net: Dict[str, torch.Tensor]
+
+    def __iter__(self):
+        return iter(
+            [self.q_net_1, self.q_net_2, self.target_q_net_1, self.target_q_net_2, self.policy_net]
+        )
+
+    def copy(self):
+        return SACModelStateDicts(
+            deepcopy(self.q_net_1),
+            deepcopy(self.q_net_2),
+            deepcopy(self.target_q_net_1),
+            deepcopy(self.target_q_net_2),
+            deepcopy(self.policy_net),
+        )
+
+
+@dataclass
+class SACModelNetworks(ModelNetworks):
+    q_net_1: network.QNetwork
+    q_net_2: network.QNetwork
+    target_q_net_1: network.QNetwork
+    target_q_net_2: network.QNetwork
+    policy_net: network.GaussianPolicy
+    device: torch.device = torch.device("cpu")
+
+    def soft_tau_update(self, sac_model_state_dicts: SACModelStateDicts, tau: float):
+        for own_state_dict, other_state_dict in zip(self.state_dicts, sac_model_state_dicts):
+
+            for own_param, other_param in zip(own_state_dict.values(), other_state_dict.values()):
+                own_param.data.copy_(tau * other_param + (1 - tau) * own_param)
+
+    def load_state_dicts(self, sac_model_state_dicts: SACModelStateDicts):
+        self.q_net_1.load_state_dict(sac_model_state_dicts.q_net_1)
+        self.q_net_2.load_state_dict(sac_model_state_dicts.q_net_2)
+        self.target_q_net_1.load_state_dict(sac_model_state_dicts.target_q_net_1)
+        self.target_q_net_2.load_state_dict(sac_model_state_dicts.target_q_net_2)
+        self.policy_net.load_state_dict(sac_model_state_dicts.policy_net)
+
+    @property
+    def state_dicts(self) -> SACModelStateDicts:
+        state_dicts = SACModelStateDicts(
+            self.q_net_1.state_dict(),
+            self.q_net_2.state_dict(),
+            self.target_q_net_1.state_dict(),
+            self.target_q_net_2.state_dict(),
+            self.policy_net.state_dict(),
+        )
+        return state_dicts
+
+    def __iter__(self):
+        return iter(
+            [self.q_net_1, self.q_net_2, self.target_q_net_1, self.target_q_net_2, self.policy_net]
+        )
 
 
 class SAC(Model):
@@ -18,19 +81,36 @@ class SAC(Model):
         policy_net: network.GaussianPolicy,
         learning_rate: float = 0.0007525,
     ) -> None:
-        self.q_net_1 = q_net_1
-        self.q_net_2 = q_net_2
-        self.policy_net = policy_net
-        self.target_q_net_1 = target_q_net_1
-        self.target_q_net_2 = target_q_net_2
+        self._nets = SACModelNetworks(q_net_1, q_net_2, target_q_net_1, target_q_net_2, policy_net)
+
         self.learning_rate = learning_rate
         self._init_optimizer(learning_rate)
         self._init_alpha(learning_rate)
 
+    @property
+    def q_net_1(self) -> network.QNetwork:
+        return self._nets.q_net_1
+
+    @property
+    def q_net_2(self) -> network.QNetwork:
+        return self._nets.q_net_2
+
+    @property
+    def target_q_net_1(self) -> network.QNetwork:
+        return self._nets.target_q_net_1
+
+    @property
+    def target_q_net_2(self) -> network.QNetwork:
+        return self._nets.target_q_net_2
+
+    @property
+    def policy_net(self) -> network.GaussianPolicy:
+        return self._nets.policy_net
+
     def _init_optimizer(self, learning_rate):
-        self.q1_optimizer = optim.Adam(self.q_net_1.parameters(), lr=learning_rate)
-        self.q2_optimizer = optim.Adam(self.q_net_2.parameters(), lr=learning_rate)
-        self.policy_optimizer = optim.Adam(self.policy_net.parameters(), lr=learning_rate / 2)
+        self.q1_optimizer = optim.Adam(self._nets.q_net_1.parameters(), lr=learning_rate)
+        self.q2_optimizer = optim.Adam(self._nets.q_net_2.parameters(), lr=learning_rate)
+        self.policy_optimizer = optim.Adam(self._nets.policy_net.parameters(), lr=learning_rate / 2)
 
     def _init_alpha(self, learning_rate):
         self.log_alpha = torch.zeros(1, requires_grad=True)
@@ -44,7 +124,7 @@ class SAC(Model):
                 flat_state, dtype=torch.float32, device=self.device
             ).unsqueeze(0)
 
-            mean, log_std, hidden_state_out = self.policy_net(flat_state, hidden_state)
+            mean, log_std, hidden_state_out = self._nets.policy_net(flat_state, hidden_state)
             std = log_std.exp()
 
             normal = Normal(mean, std)
@@ -59,76 +139,39 @@ class SAC(Model):
 
     def to(self, device: torch.device):
         self.device = device
-        self.q_net_1.to(device)
-        self.q_net_2.to(device)
-        self.target_q_net_1.to(device)
-        self.target_q_net_2.to(device)
-        self.policy_net.to(device)
+        self._nets.to(device)
         self.log_alpha = self.log_alpha.detach().to(device=device).requires_grad_()
-        # torch.ones(1, requires_grad=True, device=device.type)
         self.alpha_optim = optim.Adam([self.log_alpha], lr=self.learning_rate)
 
     def copy(self):
         copy = self.__class__(
-            self.q_net_1.copy(),
-            self.target_q_net_1.copy(),
-            self.q_net_2.copy(),
-            self.target_q_net_2.copy(),
-            self.policy_net.copy(),
+            self._nets.q_net_1.copy(),
+            self._nets.target_q_net_1.copy(),
+            self._nets.q_net_2.copy(),
+            self._nets.target_q_net_2.copy(),
+            self._nets.policy_net.copy(),
             self.learning_rate,
         )
 
         return copy
 
     def copy_shared_memory(self):
-        self.q_net_1.share_memory()
-        self.target_q_net_1.share_memory()
-        self.q_net_2.share_memory()
-        self.target_q_net_2.share_memory()
-        self.policy_net.share_memory()
+        self._nets.q_net_1.share_memory()
+        self._nets.target_q_net_1.share_memory()
+        self._nets.q_net_2.share_memory()
+        self._nets.target_q_net_2.share_memory()
+        self._nets.policy_net.share_memory()
         copy = self.__class__(
-            self.q_net_1,
-            self.target_q_net_1,
-            self.q_net_2,
-            self.target_q_net_2,
-            self.policy_net,
+            self._nets.q_net_1,
+            self._nets.target_q_net_1,
+            self._nets.q_net_2,
+            self._nets.target_q_net_2,
+            self._nets.policy_net,
             self.learning_rate,
         )
 
         return copy
 
-    def all_state_dicts(self):
-        all_state_dicts = {
-            "q_net_1": self.q_net_1.state_dict(),
-            "q_net_2": self.q_net_2.state_dict(),
-            "target_q_net_1": self.target_q_net_1.state_dict(),
-            "target_q_net_2": self.target_q_net_2.state_dict(),
-            "policy_net": self.policy_net.state_dict(),
-        }
-        return all_state_dicts
-
-    def all_parameters(self):
-        all_parameters = {
-            "q_net_1": self.q_net_1.parameters(),
-            "q_net_2": self.q_net_2.parameters(),
-            "target_q_net_1": self.target_q_net_1.parameters(),
-            "target_q_net_2": self.target_q_net_2.parameters(),
-            "policy_net": self.policy_net.parameters(),
-        }
-        return all_parameters
-
-    def load_all_state_dicts(self, all_state_dicts: dict):
-        self.q_net_1.load_state_dict(all_state_dicts["q_net_1"])
-        self.q_net_2.load_state_dict(all_state_dicts["q_net_2"])
-        self.target_q_net_1.load_state_dict(all_state_dicts["target_q_net_1"])
-        self.target_q_net_2.load_state_dict(all_state_dicts["target_q_net_2"])
-        self.policy_net.load_state_dict(all_state_dicts["policy_net"])
-
-    def soft_tau_update_all(self, all_parameters: dict, tau: float):
-
-        for net, net_key in zip(
-            [self.q_net_1, self.q_net_2, self.target_q_net_1, self.target_q_net_2, self.policy_net],
-            ["q_net_1", "q_net_2", "target_q_net_1", "target_q_net_2", "policy_net"],
-        ):
-            for self_param, extern_param in zip(net.parameters(), all_parameters[net_key]):
-                self_param.data.copy_(tau * extern_param + (1 - tau) * self_param)
+    @property
+    def nets(self) -> SACModelNetworks:
+        return self._nets

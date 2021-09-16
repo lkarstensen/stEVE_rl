52b4decbdf094e50ba6f7eed77d8817c05c42951
Lennart Karstensen <lennart.karstensen@ipa.fraunhofer.de>
2021-09-16 14:18:01 +0200

    env eve update

--- stacierl/environment/eve/lnk1.py
+++ stacierl/environment/eve/lnk1.py
@@ -4,15 +4,22 @@ import math
 
 
 class LNK1(EnvFactory):
-    def __init__(self) -> None:
-        ...
+    def __init__(
+        self, image_frequency=7.5, path_reward_factor=0.01, episode_duration_s: float = 40
+    ) -> None:
+        self.image_frequency = image_frequency
+        self.path_reward_factor = path_reward_factor
+        self.n_max_steps = episode_duration_s * image_frequency
 
     def create_env(self) -> eve.Env:
         tree_name = eve.vesseltree.CAD_VesselTrees.ORGANIC_V2
         vessel_tree = eve.vesseltree.CAD(vessel_tree_name=tree_name)
         device = eve.device.Guidewire()
         simulation = eve.simulation.SofaPy3(
-            device=device, vessel_tree=vessel_tree, sofa_native_gui=False
+            device=device,
+            vessel_tree=vessel_tree,
+            sofa_native_gui=False,
+            image_frequency=self.image_frequency,
         )
         start = eve.start.InsertionPoint()
         target = eve.target.CenterlineRandom(vessel_tree, simulation, target_threshold=10)
@@ -40,10 +47,10 @@ class LNK1(EnvFactory):
 
         target_reward = eve.reward.TargetReached(target, factor=1.0)
         # step_reward = eve.reward.Step(factor=-0.01)
-        path_delta = eve.reward.PathLengthDelta(pathfinder, 0.01)
+        path_delta = eve.reward.PathLengthDelta(pathfinder, self.path_reward_factor)
         reward = eve.reward.Combination([target_reward, path_delta])
 
-        max_steps = eve.done.MaxSteps(200)
+        max_steps = eve.done.MaxSteps(self.n_max_steps)
         target_reached = eve.done.TargetReached(target)
         done = eve.done.Combination([max_steps, target_reached])
 

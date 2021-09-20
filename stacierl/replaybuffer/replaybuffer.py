9b5f6998cd52d734a73778ac75b3465a71723d4d
Lennart Karstensen <lennart.karstensen@ipa.fraunhofer.de>
2021-09-20 10:20:13 +0200

    replaybuffer: base update

--- stacierl/replaybuffer/replaybuffer.py
+++ stacierl/replaybuffer/replaybuffer.py
@@ -1,18 +1,33 @@
 from abc import ABC, abstractmethod
-from typing import List, NamedTuple
+from typing import List, NamedTuple, Union
 import numpy as np
+from torch.nn.utils.rnn import PackedSequence
+
+import torch
+
 from dataclasses import dataclass
+
+
+@dataclass
+class EpisodeNumpy:
+
+    states: np.ndarray
+    actions: np.ndarray
+    rewards: np.ndarray
+    next_states: np.ndarray
+    dones: np.ndarray
+
+    def __len__(self):
+        return len(self.states)
 
 
 class Episode:
     def __init__(self) -> None:
         self.states: List[np.ndarray] = []
         self.actions: List[np.ndarray] = []
-        self.rewards: List[float] = []
+        self.rewards: List[np.ndarray] = []
         self.next_states: List[np.ndarray] = []
-        self.dones: List[bool] = []
-        self.hidden_states: List[np.ndarray] = []
-        self.next_hidden_states: List[np.ndarray] = []
+        self.dones: List[np.ndarray] = []
 
     def add_transition(
         self,
@@ -21,39 +36,46 @@ class Episode:
         reward: float,
         next_state: np.ndarray,
         done: bool,
-        hidden_state: np.ndarray = None,
-        next_hidden_state: np.ndarray = None,
     ):
         self.states.append(state)
         self.actions.append(action)
-        self.rewards.append(reward)
+        self.rewards.append(np.array([reward]))
         self.next_states.append(next_state)
-        self.dones.append(done)
-        if hidden_state:
-            self.hidden_states.append(hidden_state)
-            self.next_hidden_states.append(next_hidden_state)
+        self.dones.append(np.array([done]))
 
     def __len__(self):
         return len(self.states)
 
+    def to_numpy(self) -> EpisodeNumpy:
+        return EpisodeNumpy(
+            states=np.array(self.states),
+            actions=np.array(self.actions),
+            rewards=np.array(self.rewards),
+            next_states=np.array(self.next_states),
+            dones=np.array(self.dones),
+        )
+
 
 class Batch(NamedTuple):
-    states: np.ndarray
-    actions: np.ndarray
-    rewards: np.ndarray
-    next_states: np.ndarray
-    dones: np.ndarray
-    hidden_states: np.ndarray = None
-    hidden_next_states: np.ndarray = None
+    states: PackedSequence
+    actions: PackedSequence
+    rewards: PackedSequence
+    next_states: PackedSequence
+    dones: PackedSequence
 
 
 class ReplayBuffer(ABC):
+    @property
+    @abstractmethod
+    def batch_size(self) -> int:
+        ...
+
     @abstractmethod
     def push(self, episode: Episode) -> None:
         ...
 
     @abstractmethod
-    def sample(self, batch_size: int) -> Batch:
+    def sample(self) -> Batch:
         ...
 
     @abstractmethod
@@ -61,5 +83,9 @@ class ReplayBuffer(ABC):
         ...
 
     @abstractmethod
-    def close(self):
+    def close(self) -> None:
+        ...
+
+    @abstractmethod
+    def __len__(self) -> int:
         ...

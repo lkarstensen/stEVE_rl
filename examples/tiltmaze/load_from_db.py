2dc9a69cc15c16c216c20c2e3613bf8987e2964b
Jacqueline Ritter <jacqueline.ritter@ipa.fraunhofer.de>
2022-03-21 09:44:16 +0100

    examples.tiltmaze.load_from_db.py: bugfix

--- examples/tiltmaze/load_from_db.py
+++ examples/tiltmaze/load_from_db.py
@@ -1,14 +1,13 @@
 import stacierl
 from stacierl.replaybuffer.wrapper import FilterElement, FilterMethod
 
-replay_buffer = 1e6,
+replay_buffer = 1e6
 batch_size = 64
 
 db_filter = [
     FilterElement('success', 1, FilterMethod.GREATEREQUAL),
     FilterElement('simulation.tip_length', 25, FilterMethod.EXACT)
 ]
-
 replay_buffer = stacierl.replaybuffer.VanillaStepDB(replay_buffer, batch_size)
 replay_buffer = stacierl.replaybuffer.LoadFromDB(nb_loaded_episodes=10,
                                                  db_filter=db_filter, 

3a193f95b08507b9561730f0a97de60679ebb7d8
Lennart Karstensen <lennart.karstensen@ipa.fraunhofer.de>
2022-11-03 12:06:27 +0000

    example update

--- examples/tiltmaze/fast_learner.py
+++ examples/tiltmaze/fast_learner.py
@@ -1,11 +1,9 @@
 import stacierl
-import tiltmaze
 import eve
 import torch
 from datetime import datetime
 import csv
 import os
-import math
 
 
 def sac_training(
@@ -30,39 +28,30 @@ def sac_training(
     if not os.path.isdir(log_folder):
         os.mkdir(log_folder)
     success = 0.0
-    eve_tree = eve.vesseltree.AorticArch(seed=1234)
-    vessel_tree = tiltmaze.vesseltree.FromEve3d(
-        eve3d_vessel_tree=eve_tree,
-        lao_rao=20,
-        cra_cau=-5,
-        contour_approx_margin=2.0,
-        scaling_artery_diameter=0.5,
-    )
-    simu = tiltmaze.simulation.Guidewire(
-        tip_length=25,
-        tip_angle=math.pi / 2,
-        flex_length=30,
-        flex_element_length=1,
-        flex_rotary_spring_stiffness=7e5,
-        flex_rotary_spring_damping=3e2,
-        stiff_element_length=2,
-        stiff_rotary_spring_stiffness=1e6,
-        stiff_rotary_spring_damping=3e2,
-        wire_diameter=2,
+    vessel_tree = eve.vesseltree.AorticArch(seed=1234)
+    vessel_tree.rotate(z_deg=20, x_deg=-5)
+    vessel_tree.to_2d(axis_to_remove="y")
+    vessel_tree.scale(1, 1, 1, 0.5)
+
+    instrument = eve.simulation2d.device.JWire()
+
+    simu = eve.simulation2d.SingleDevice(
+        instrument,
+        velocity_limit=(50, 1.5),
+        element_length=1,
+        image_frequency=7.5,
+        dt_simulation=0.0002,
         friction=1.0,
         damping=0.000001,
-        velocity_limits=(50, 1.5),
-        normalize_action=True,
-        dt_step=1 / 7.5,
-        dt_simulation=0.0002,
         body_mass=0.01,
         body_moment=0.1,
-        spring_stiffness=2.5e6,
-        spring_damping=100,
+        linear_stiffness=2.5e6,
+        linear_damping=100,
         last_segment_kp_angle=3,
         last_segment_kp_translation=5,
     )
-    target = tiltmaze.target.CenterlineRandom(
+
+    target = eve.target.CenterlineRandom(
         5,
         branches=[
             "right subclavian artery",
@@ -72,31 +61,31 @@ def sac_training(
             "brachiocephalic trunk",
         ],
     )
-    pathfinder = tiltmaze.pathfinder.BruteForceBFS()
-    start = tiltmaze.start.InsertionPoint()
-    imaging = tiltmaze.imaging.ImagingDummy((500, 500))
+    pathfinder = eve.pathfinder.BruteForceBFS()
+    start = eve.start.InsertionPoint()
+    imaging = eve.imaging.ImagingDummy((500, 500))
 
-    pos = tiltmaze.state.Position(n_points=6, resolution=1)
-    pos = tiltmaze.state.wrapper.Normalize(pos)
-    target_state = tiltmaze.state.Target()
-    target_state = tiltmaze.state.wrapper.Normalize(target_state)
-    rot = tiltmaze.state.Rotation()
-    state = tiltmaze.state.Combination([pos, target_state, rot])
+    pos = eve.state.Tracking(n_points=6, resolution=1)
+    pos = eve.state.wrapper.Normalize(pos)
+    target_state = eve.state.Target()
+    target_state = eve.state.wrapper.Normalize(target_state)
+    rot = eve.state.Rotation()
+    state = eve.state.Combination([pos, target_state, rot])
 
-    target_reward = tiltmaze.reward.TargetReached(1.0)
-    step_reward = tiltmaze.reward.Step(-0.005)
-    path_length_reward = tiltmaze.reward.PathLengthDelta(0.001)
-    reward = tiltmaze.reward.Combination([target_reward, step_reward, path_length_reward])
+    target_reward = eve.reward.TargetReached(1.0)
+    step_reward = eve.reward.Step(-0.005)
+    path_length_reward = eve.reward.PathLengthDelta(0.001)
+    reward = eve.reward.Combination([target_reward, step_reward, path_length_reward])
 
-    done_target = tiltmaze.done.TargetReached()
-    done_steps = tiltmaze.done.MaxSteps(100)
-    done = tiltmaze.done.Combination([done_target, done_steps])
+    done_target = eve.done.TargetReached()
+    done_steps = eve.done.MaxSteps(100)
+    done = eve.done.Combination([done_target, done_steps])
 
-    success = tiltmaze.success.TargetReached()
-    visu = tiltmaze.visualisation.VisualisationDummy()
+    success = eve.success.TargetReached()
+    visu = eve.visualisation.VisualisationDummy()
 
-    randomizer = tiltmaze.randomizer.RandomizerDummy()
-    env = tiltmaze.Env(
+    randomizer = eve.randomizer.RandomizerDummy()
+    env = eve.Env(
         vessel_tree=vessel_tree,
         state=state,
         reward=reward,
@@ -125,7 +114,13 @@ def sac_training(
     algo = stacierl.algo.SAC(sac_model, action_space=env.action_space, gamma=gamma)
     replay_buffer = stacierl.replaybuffer.VanillaStep(replay_buffer, batch_size)
     agent = stacierl.agent.Single(
-        algo, env, env, replay_buffer, consecutive_action_steps=1, device=device
+        algo,
+        env,
+        env,
+        replay_buffer,
+        consecutive_action_steps=1,
+        device=device,
+        normalize_actions=True,
     )
 
     while True:
@@ -142,8 +137,10 @@ def sac_training(
     # agent.load_checkpoint(log_folder, "checkpoint_10053.pt")
     next_eval_step_limt = steps_between_eval + agent.step_counter.exploration
     training_steps += agent.step_counter.exploration
-    agent.heatup(steps=heatup, custom_action_low=[0.0, -1.0])
+    print("starting heatup")
+    agent.heatup(steps=heatup, custom_action_low=[-10.0, -1.5])
     step_counter = agent.step_counter
+    print("starting training loop")
     while step_counter.exploration < training_steps:
         agent.explore(steps=consecutive_explore_steps)
         step_counter = agent.step_counter
@@ -151,7 +148,7 @@ def sac_training(
         agent.update(update_steps)
 
         if step_counter.exploration >= next_eval_step_limt:
-            agent.save_checkpoint(log_folder, f"checkpoint_{step_counter.exploration}")
+            # agent.save_checkpoint(log_folder, f"checkpoint_{step_counter.exploration}")
             episodes = agent.evaluate(episodes=eval_episodes)
             rewards = [episode.episode_reward for episode in episodes]
             successes = [episode.episode_success for episode in episodes]
@@ -159,7 +156,9 @@ def sac_training(
             success = sum(successes) / len(successes)
             next_eval_step_limt += steps_between_eval
 
-            print(f"Steps: {step_counter.exploration}, Reward: {reward}, Success: {success}")
+            print(
+                f"Steps: {step_counter.exploration}, Reward: {reward}, Success: {success}"
+            )
             with open(logfile, "a+", newline="") as csvfile:
                 writer = csv.writer(csvfile, delimiter=";")
                 writer.writerow(

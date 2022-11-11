7b245f8c8eb5117602d1d5000778e68b18d37d5b
Lennart Karstensen <lennart.karstensen@ipa.fraunhofer.de>
2022-11-11 16:35:49 +0100

    agent.synchron: log msgs and worker restart on exception

--- stacierl/agent/synchron.py
+++ stacierl/agent/synchron.py
@@ -9,7 +9,6 @@ from eve.env import DummyEnv
 from math import ceil, inf
 import torch
 import os
-from time import sleep
 
 
 class Synchron(Agent):
@@ -48,22 +47,7 @@ class Synchron(Agent):
         self._episode_counter = EpisodeCounterShared()
 
         for i in range(n_worker):
-            self.worker.append(
-                SingleAgentProcess(
-                    i,
-                    algo.copy(),
-                    deepcopy(env_train),
-                    deepcopy(env_eval),
-                    replay_buffer.copy(),
-                    worker_device,
-                    consecutive_action_steps,
-                    normalize_actions,
-                    name="worker_" + str(i),
-                    parent_agent=self,
-                    step_counter=self.step_counter,
-                    episode_counter=self.episode_counter,
-                )
-            )
+            self.worker.append(self._create_worker_agent(i))
 
         for i in range(n_trainer):
             if share_trainer_model:
@@ -103,10 +87,7 @@ class Synchron(Agent):
         custom_action_low: List[float] = None,
         custom_action_high: List[float] = None,
     ) -> List[Episode]:
-        self.logger.debug(f"heatup: {steps} steps / {episodes} episodes")
-        steps_per_agent, episodes_per_agent = self._divide_steps_and_episodes(
-            steps, episodes, self.n_worker
-        )
+        self.logger.info(f"heatup: {steps} steps / {episodes} episodes")
         for agent in self.worker:
             agent.heatup(
                 steps,
@@ -118,19 +99,15 @@ class Synchron(Agent):
         return result
 
     def explore(self, steps: int = inf, episodes: int = inf) -> List[Episode]:
-        self.logger.debug(f"explore: {steps} steps / {episodes} episodes")
+        self.logger.info(f"explore: {steps} steps / {episodes} episodes")
 
-        steps_per_agent, episodes_per_agent = self._divide_steps_and_episodes(
-            steps, episodes, self.n_worker
-        )
         for agent in self.worker:
             agent.explore(steps, episodes)
         result = self._get_worker_results()
         return result
 
     def update(self, steps) -> List[float]:
-        self.logger.debug(f"update: {steps} steps")
-        steps_per_agent = ceil(steps / self.n_trainer)
+        self.logger.info(f"update: {steps} steps")
         for agent in self.trainer:
             agent.update(steps)
         result = self._get_trainer_results()
@@ -139,11 +116,8 @@ class Synchron(Agent):
         return result
 
     def evaluate(self, steps: int = inf, episodes: int = inf) -> List[Episode]:
-        self.logger.debug(f"evaluate: {steps} steps / {episodes} episodes")
+        self.logger.info(f"evaluate: {steps} steps / {episodes} episodes")
 
-        steps_per_agent, episodes_per_agent = self._divide_steps_and_episodes(
-            steps, episodes, self.n_worker
-        )
         for agent in self.worker:
             agent.evaluate(steps, episodes)
 
@@ -153,21 +127,18 @@ class Synchron(Agent):
     def explore_and_update_parallel(
         self, update_steps: int, explore_steps: int = inf, explore_episodes: int = inf
     ) -> Tuple[List[Episode], List[float]]:
-        update_steps_per_agent = ceil(update_steps / self.n_trainer)
-        (
-            explore_steps_per_agent,
-            explore_episodes_per_agent,
-        ) = self._divide_steps_and_episodes(
-            explore_steps, explore_episodes, self.n_worker
+        self.logger.info(
+            f"explore: {explore_steps} steps / {explore_episodes} episodes, update: {update_steps} steps "
         )
+
         for agent in self.trainer:
             agent.update(update_steps)
         for agent in self.worker:
             agent.explore(explore_steps, explore_episodes)
         update_result = self._get_trainer_results()
         explore_result = self._get_worker_results()
-        new_network_states = self._get_network_states_container()
-        self._set_network_states_container(new_network_states)
+        self.latest_network_states = self._get_network_states_container()
+        self._set_network_states_container(self.latest_network_states)
 
         return explore_result, update_result
 
@@ -222,8 +193,19 @@ class Synchron(Agent):
 
     def _get_worker_results(self):
         episodes = []
-        for agent in self.worker:
-            episodes += agent.get_result()
+        for i, agent in enumerate(self.worker):
+            try:
+                episodes += agent.get_result()
+            except Exception as e:
+                self.logger.warning(
+                    f"Restaring Agent {agent.name} because of Exception {e}"
+                )
+                agent.close()
+                new_agent = self._create_worker_agent(i)
+                new_agent.set_network_states(self.latest_network_states)
+                self.worker[i] = new_agent
+                continue
+
         return episodes
 
     def _get_trainer_results(self):
@@ -239,6 +221,22 @@ class Synchron(Agent):
             if val is not None
         ]
         return results
+
+    def _create_worker_agent(self, i):
+        return SingleAgentProcess(
+            i,
+            self.algo.copy(),
+            deepcopy(self.env_train),
+            deepcopy(self.env_eval),
+            self.replay_buffer.copy(),
+            self.worker_device,
+            self.consecutive_action_steps,
+            self.normalize_actions,
+            name="worker_" + str(i),
+            parent_agent=self,
+            step_counter=self.step_counter,
+            episode_counter=self.episode_counter,
+        )
 
     # @property
     # def step_counter(self) -> StepCounter:

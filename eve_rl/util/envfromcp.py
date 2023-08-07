3ec201d2ef384a3920fba73ac800aa8fe372975e
Lennart Karstensen <lennart.karstensen@ipa.fraunhofer.de>
2023-08-07 10:39:52 +0200

    util.envfromcp: implemented "to_exchange"

--- eve_rl/util/envfromcp.py
+++ eve_rl/util/envfromcp.py
@@ -1,9 +1,13 @@
 from importlib import import_module
+from typing import Dict
 import torch
+from eve.util.eveobject import EveObject
 
 
-def get_env_from_checkpoint(checkpoint_path: str, mode: str = "eval"):
+def get_env_from_checkpoint(
+    checkpoint_path: str, mode: str = "eval", to_exchange: Dict[str, EveObject] = None
+):
     cp = torch.load(checkpoint_path)
     env_dict = cp[f"env_{mode}"]
     eve = import_module("eve")
-    return eve.Env.from_config_dict(env_dict)
+    return eve.Env.from_config_dict(env_dict, to_exchange)

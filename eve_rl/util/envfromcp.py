da21f71ac53f9357e5a0c2509b53281aa7c90b8b
Lennart Karstensen <lennart.karstensen@ipa.fraunhofer.de>
2023-11-01 08:47:32 +0100

    util.envfromcp: cleanup

--- eve_rl/util/envfromcp.py
+++ eve_rl/util/envfromcp.py
@@ -1,13 +1,12 @@
-from importlib import import_module
 from typing import Dict
 import torch
 from eve.util.eveobject import EveObject
+import eve
 
 
 def get_env_from_checkpoint(
     checkpoint_path: str, mode: str = "eval", to_exchange: Dict[str, EveObject] = None
-):
+) -> eve.Env:
     cp = torch.load(checkpoint_path)
     env_dict = cp[f"env_{mode}"]
-    eve = import_module("eve")
     return eve.Env.from_config_dict(env_dict, to_exchange)
